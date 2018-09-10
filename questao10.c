605e597820180c875480c652bf9a2281f4f61462
Malu <40304313+maluiza@users.noreply.github.com>
2018-09-10 12:09:43 -0300

    questao10

--- questao10.c
+++ questao10.c
@@ -1,1 +1,16 @@
-
+Para quando x dor declarado como char:
+		x+1 = 4093
+		x+2 = 4094
+		x+3 = 4095
+	Para quando x for declarado como int:
+		x+1 = 4094
+		x+2 = 4096
+		x+3 = 4098
+	Para quando x for declarado como float:
+		x+1 = 4096
+		x+2 = 4100
+		x+3 = 4104
+	Para quando x for declarado como double:
+		x+1 = 4100
+		x+2 = 4108
+		x+3 = 4116
