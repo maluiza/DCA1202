68d06464cba5827bfbe7cb99264b3c5cf06fbee0
Malu <40304313+maluiza@users.noreply.github.com>
2018-09-10 11:52:21 -0300

    questao8

--- questao8.c
+++ questao8.c
@@ -1,1 +1,5 @@
-
+As expressões p = mat + 1 (nesse caso está caminhando
+um tamanho de ponteiro para int) e x = (*mat)++(incrementa 
+o conteúdo da matriz),pois nos demais exemplos(p = mat++ e 
+p = ++mat) não é válida a forma que está sendo incrementado
+o vetor mat.
