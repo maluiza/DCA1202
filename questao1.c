327f28a3e091d7d462b75a7e8f60a0359be8d881
Malu <40304313+maluiza@users.noreply.github.com>
2018-09-10 08:15:34 -0300

    questao1

--- questao1.c
+++ questao1.c
@@ -1,1 +1,3 @@
-
+De maneira mais resumida, a utilização de ponteiros permite que vc gaste menos memória durante a execução
+do programa. De uma forma mais técnica, podemos citar a Alocação dinâmica de memória e também aPassagem por 
+referência.
