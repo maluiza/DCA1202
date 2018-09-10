a4e024ff0249816e41ab27011592182dd8b7869a
Malu <40304313+maluiza@users.noreply.github.com>
2018-09-10 11:59:13 -0300

    questao9

--- questao9.c
+++ questao9.c
@@ -1,1 +1,5 @@
+O primeiro programa inicialmente cria um vetor (vet) e atribui valores para ele, em seguida é usado o laço
+ de repetição for para imprimir o valor, em decimal, salvo em vet.
 
+ No segundo vetor,acontece algo semelhante, porém, o conteúdo impresso é o local na memória(endereço) onde está cada uma
+ das informações impressas no programa anterior.
