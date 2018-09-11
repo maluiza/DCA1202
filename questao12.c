5cb717b3ad008ccaef0e0c272dcb87f0e1138084
Malu <40304313+maluiza@users.noreply.github.com>
2018-09-10 21:16:06 -0300

    questao12

--- questao12.c
+++ questao12.c
@@ -1,1 +1,8 @@
-
+aloha[2] = value; //comando valido
+scanf("%f", &aloha); //comando invalido
+aloha = value";//comando invalido
+printf("%f", aloha); //comando invalido
+coisas[4][4] = aloha[3]; //comando valido
+coisas[5] = aloha; //comando invalido
+pf = value; //comando invalido
+pf = aloha; //comando valido
