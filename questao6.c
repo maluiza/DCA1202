b2eb51c2ad59cd9caac797c6fa5fe7d58d1a8991
Malu <40304313+maluiza@users.noreply.github.com>
2018-09-10 22:21:52 -0300

    questao6

--- questao6.c
+++ questao6.c
@@ -1,1 +1,5 @@
-
+i = 0vet[0] = 1.1*(f + 0) = 1.1&vet[0] = FC0C3ED0(f + 0) = FC0C3ED0
+i = 1vet[1] = 2.2*(f + 1) = 2.2&vet[1] = FC0C3ED4(f + 1) = FC0C3ED4
+i = 2vet[2] = 3.3*(f + 2) = 3.3&vet[2] = FC0C3ED8(f + 2) = FC0C3ED8
+i = 3vet[3] = 4.4*(f + 3) = 4.4&vet[3] = FC0C3EDC(f + 3) = FC0C3EDC
+i = 4vet[4] = 5.5*(f + 4) = 5.5&vet[4] = FC0C3EE0(f + 4) = FC0C3EE0
