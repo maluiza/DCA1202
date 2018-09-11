e712e2579e0544bf66969642875224cc65dbdc82
Malu <40304313+maluiza@users.noreply.github.com>
2018-09-10 22:54:46 -0300

    questao10

--- questao10.c
+++ questao10.c
@@ -1,16 +1,27 @@
 Para quando x dor declarado como char:
+para o tipo char, cada elemento vai pular 1 byte do endereco da memória após ser incrementado
+
 	x+1 = 4093
 	x+2 = 4094
 	x+3 = 4095
+
 Para quando x for declarado como int:
+para o tipo int, cada elemento vai pular 2 bytes do endereco da memória após ser incrementado
+
 	x+1 = 4094
 	x+2 = 4096
 	x+3 = 4098
+	
 Para quando x for declarado como float:
+para o tipo float, cada elemento vai pular 4 bytes do endereco da memória após ser incrementado
+
 	x+1 = 4096
 	x+2 = 4100
 	x+3 = 4104
+	
 Para quando x for declarado como double:
+para o tipo double, cada elemento vai pular 8 bytes do endereco da memória após ser incrementado
+
 	x+1 = 4100
 	x+2 = 4108
 	x+3 = 4116
