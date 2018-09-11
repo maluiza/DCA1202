1e65f78010ab949c47b405dff27eb50ca46c5bfa
Malu <40304313+maluiza@users.noreply.github.com>
2018-09-10 21:17:15 -0300

    questao13

--- questao13.c
+++ questao13.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+int TrocarNumeros (int &n1, int &n2) {
+	int aux;
+	aux = n1;
+	n1 = n2;
+	n2 = aux;
+}
 
+int main () {
+	int num1 = 12;
+	int num2 = 63;
+	
+	TrocarNumeros (num1, num2);
+	printf("Valor de 1:", num1);
+	printf("Valor de 2:", num2);
+	
+
+return 0;
+}
